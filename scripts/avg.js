0f07bd9490f7f779f1eb3821822d20500daf8b3c
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-06 10:59:12 +0200

    Added some function examples

--- scripts/avg.js
+++ scripts/avg.js
@@ -1,13 +1,39 @@
+console.log("Starting work", 6 ** 2);
+let a = 1000; //global a
+//giving a default value to function argument
+function printStuff(a = 50) {
+    console.log("Trying to print a", a);
+    let b = a + 5;
+    let c = a + 6;
+
+    console.log(`Hello Valdis ${a * 82} and there more to know about ${b}`);
+    console.log(`Hello Liga`, b * 2);
+    console.log(`Hello Peter Mr.` + c + " " + b);
+}
+
 function randomRange(myMin, myMax) {
     return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
 }
 
 function getAverage(numThrows) {
     //write function to get an average value from dice throws
-    return 3.5;
+    let total = 0;
+    //write loop here
+    // use randomRange(1, 6);
+    //loop goes here
+    for (let i = 0; i < numThrows; i++) {
+        total += randomRange(1, 6);
+    }
+
+    return total / numThrows;
 }
 
-let result = getAverage(1000);
+const numThrows = 100000;
+let result = getAverage(numThrows);
+
+console.log("Going to print answer");
 
 let myParagraph = document.querySelector("#answer");
-myParagraph.textContent = `Average throw value is ${result}`;
+// let myParagraph = document.querySelector("p");
+myParagraph.myResult = result;
+myParagraph.textContent = `Average throw value is ${myParagraph.myResult}`;
