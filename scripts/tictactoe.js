765d120a1e9ebe2fb41e6b29e5a86cc37a594580
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-07 11:58:38 +0200

    AddGameRenderandTurns

--- scripts/tictactoe.js
+++ scripts/tictactoe.js
@@ -2,9 +2,28 @@ console.log("Started TicTacToe");
 // const squares = document.querySelectorAll(".square");
 //same as above
 const squares = document.getElementsByClassName("square");
+const status = document.getElementById("game-status");
+let turn = "Player A";
 
-function onClick() {
-    console.log("Clicked some button");
+function renderGame() {
+    status.textContent = `${turn} goes now`;
+    //TODO update game screen
+}
+
+function onClick(event) {
+    console.log("Event of type", event.type);
+    console.log("My id is", event.currentTarget.id);
+    //this gets us the last character
+    console.log("My number is", event.currentTarget.id.slice(-1));
+    //this gets us whatever is after - in id
+    console.log("My number is", event.currentTarget.id.split("-")[1]);
+
+    if (turn === "Player A") {
+        turn = "Player B";
+    } else {
+        turn = "Player A";
+    }
+    renderGame();
 }
 
 function addListeners() {
@@ -13,8 +32,9 @@ function addListeners() {
     console.log(squares.length);
     for (let i = 0; i < squares.length; i++) {
         const sq = squares[i];
-        console.log("My id is", sq.id);
-        sq.addEventListener("click", () => console.log("Clicked me!"));
+        // console.log("My id is", sq.id);
+        sq.setAttribute("data-value", i + 1);
+        sq.addEventListener("click", onClick);
     }
 }
 
