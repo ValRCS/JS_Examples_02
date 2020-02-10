7a550203e43f424e7fac7c4d02469a2730fb21ec
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-10 13:01:24 +0200

    Added Fetch from Outside

--- scripts/todo.js
+++ scripts/todo.js
@@ -4,6 +4,40 @@ const jobsCont = document.querySelector(".jobs-cont");
 const createBtn = document.querySelector("#create-btn");
 const clearBtn = document.querySelector("#clear-btn");
 
+let jobs = [{
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+},
+{
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+}
+];
+
+function createSingleJob(parent, data) {
+    const jobDiv = document.createElement("div");
+    const descSpan = document.createElement("span");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "Delete current row";
+    delBtn.addEventListener("click", onDeleteCurrent);
+    descSpan.innerText = data.title; //because we have title property in our data
+    jobDiv.appendChild(descSpan);
+    jobDiv.appendChild(delBtn);
+    parent.appendChild(jobDiv);
+}
+
+function render() {
+    console.log("Render Jobs");
+    for (let i = 0; i < jobs.length; i++) {
+        console.log("Adding job with id:", jobs[i].id);
+        createSingleJob(jobsCont, jobs[i]);
+    }
+}
+
 function onDeleteCurrent(event) {
     console.log("Deleting current item", event.currentTarget.innerText);
     const parentDiv = event.currentTarget.parentElement;
@@ -13,15 +47,26 @@ function onDeleteCurrent(event) {
 
 function onCreate() {
     console.log("Adding new TODO item! Using description", descInp.value);
-    const jobDiv = document.createElement("div");
-    const descSpan = document.createElement("span");
-    const delBtn = document.createElement("button");
-    delBtn.innerText = "Delete current row";
-    delBtn.addEventListener("click", onDeleteCurrent);
-    descSpan.innerText = descInp.value;
-    jobDiv.appendChild(descSpan);
-    jobDiv.appendChild(delBtn);
-    jobsCont.appendChild(jobDiv);
+    const lastEl = jobs[jobs.length - 1]; //will not work on empty list
+
+    const jobObj = {
+        "title": descInp.value,
+        "id": lastEl.id + 1,
+        "userId": lastEl.userId,
+        "completed": false
+    }
+    jobs = jobs.concat(jobObj);
+    createSingleJob(jobsCont, jobObj);
+    // createSingleJob(jobsCont, { "title": descInp.value });
+    // const jobDiv = document.createElement("div");
+    // const descSpan = document.createElement("span");
+    // const delBtn = document.createElement("button");
+    // delBtn.innerText = "Delete current row";
+    // delBtn.addEventListener("click", onDeleteCurrent);
+    // descSpan.innerText = descInp.value;
+    // jobDiv.appendChild(descSpan);
+    // jobDiv.appendChild(delBtn);
+    // jobsCont.appendChild(jobDiv);
 }
 
 function onClearAll() {
@@ -38,9 +83,24 @@ function addListeners() {
     clearBtn.addEventListener("click", onClearAll);
 }
 
+function processJSON(json) {
+    console.log("We got entries count:", json.length);
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+    jobs = jobs.concat(json);
+    render();
+}
+function fetchOutsideData(url) {
+    console.log("Fetching Outside Data");
+    fetch(url)
+        .then(response => response.json())
+        .then(processJSON);
+}
+
 function main() {
     console.log("Running Main");
     addListeners();
+    render();
+    fetchOutsideData('https://jsonplaceholder.typicode.com/todos');
 }
 
 // window.onload = main; //not necessary with defer in our script
