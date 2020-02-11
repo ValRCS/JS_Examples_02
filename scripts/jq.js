febc479fc1d8af1ebbed1e2b2a9f01e2dd456547
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-11 12:21:20 +0200

    Removing specific elements

--- scripts/jq.js
+++ scripts/jq.js
@@ -10,4 +10,16 @@ function main() {
     $('h1').css('background-color', 'silver');
     $('h1').addClass('headline-main');
     $('.j-cont').append($("<span>My text</span>").addClass("widget"));
+    $('.j-cont').append($("<button>Press Me</button>").addClass("btn-primary"));
+    $('.j-cont').append($("<button>Press Me Too</button>").addClass("btn-secondary"));
+
+    $('.btn-primary').prop("disabled", true);
+    $('.btn-primary').prop("name", "button-name");
+    $('.btn-primary').remove();
+    document.querySelectorAll('.btn-secondary').forEach((el) => { el.setAttribute("disabled", true) }
+
+    );
+    document.querySelectorAll('.btn-secondary').forEach((el) => {
+        el.parentNode.removeChild(el);
+    });
 }
