0236577dac99d174eed9859223b8bc410b13da3b
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-12 12:25:04 +0200

    added Vue v-for examples

--- scripts/myvue.js
+++ scripts/myvue.js
@@ -1,9 +1,22 @@
 console.log("Running my View");
 
+
 const app = new Vue({
     el: '#app',
     data: {
         message: 'RCS!',
-        mynum: 5
+        mynum: 5,
+        mydate: new Date(),
+        seen: true,
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ],
+        jobs: [
+            "Buy Milk",
+            "Buy potatoes",
+            "Clean the garage!"
+        ]
     }
 })
