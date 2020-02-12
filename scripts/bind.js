8d05c6643db36309e3f64af8c590d169fd4c3832
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-12 11:27:59 +0200

    Two Way Binding

--- scripts/bind.js
+++ scripts/bind.js
@@ -8,12 +8,22 @@ function onChange(event) {
     console.log("My Value is", event.target.value);
     myNum.value = event.target.value;
 }
+
 function onInput(event) {
     console.log("My Input Value is", event.target.value);
     myNum.value = event.target.value;
 }
 
+function onNumInp(event) {
+    console.log("Num Input Value Input", event.target.value);
+
+    myRange.value = event.target.value;
+}
+
+myNum.addEventListener('input', onNumInp);
+
 // myRange.addEventListener('change', () => console.log("I was changed"));
 myRange.addEventListener('change', onChange);
 // myRange.addEventListener('input', (ev) => console.log("V:", ev.target.value));
-myRange.addEventListener('input', onInput);
+// myRange.addEventListener('input', onInput);
+
