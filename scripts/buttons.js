3642e4b4ae107709557621a4c1ec046835bc04c6
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-14 12:35:39 +0200

    Adding multiple buttons dynamically

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -5,7 +5,8 @@ const config = {
 const state = {
     cnt: 0,
     quickcnt: 0,
-    mcnt: 0
+    mcnt: 0,
+    lastBtnId: 0
 }
 let timer;
 
@@ -15,8 +16,8 @@ let timer;
 const btns = document.querySelectorAll("button");
 const btnsAgain = document.getElementsByTagName("button");
 const btnsAlso = document.getElementsByClassName("btn");
-
-
+const statusDisp = document.querySelector("#status-p");
+const area = document.querySelector("#my-area");
 //individual buttons
 const b1 = document.querySelector("#b-1");
 const b2 = document.querySelector(".btn-2");
@@ -27,6 +28,31 @@ const p1 = document.querySelector(".p-1");
 const p2 = document.querySelector(".p-2");
 const p3 = document.querySelector(".p-3");
 const p4 = document.querySelector(".p-4");
+
+const genContainer = document.querySelector(".gen-cont");
+
+function generateButtons(cnt) {
+    for (let i = 0; i < cnt; i++) {
+        let btn = document.createElement('button');
+        btn.id = "btn-" + (i + state.lastBtnId);
+        btn.classList.add('nice-btn', 'btn');
+        btn.value = i + state.lastBtnId;
+        btn.textContent = 'B' + (i + state.lastBtnId);
+        btn.addEventListener("click", (event) => {
+            console.log("Button pressed:",
+                event.target.id,
+                event.target.value,
+                event.target.textContent
+            );
+            statusDisp.textContent = "You pressed Button #" + event.target.value;
+        })
+
+        genContainer.appendChild(btn);
+    }
+    state.lastBtnId += cnt;
+}
+
+
 console.log(state.cnt);
 p1.textContent = "P1: " + state.cnt;
 
@@ -68,13 +94,20 @@ function onMouseMove() {
 }
 b3.addEventListener("mousemove", onMouseMove);
 
-const area = document.querySelector("#my-area");
-
-area.addEventListener("mousemove", (event) => {
-    console.log("Moving mouse");
-    // p4.textContent = `X:(${window.screenX}) Y:(${window.screenY})`;
-    p4.textContent = `X:(${event.screenX}) Y:(${event.screenY})`;
-})
 
 
+
+
+function addEventListeners() {
+    area.addEventListener("mousemove", (event) => {
+        console.log("Moving mouse");
+        // p4.textContent = `X:(${window.screenX}) Y:(${window.screenY})`;
+        p4.textContent = `X:(${event.screenX}) Y:(${event.screenY})`;
+    })
+
+
+    btns[4].addEventListener("click", () => {
+        generateButtons(state.quickcnt);
+    });
+}
 
