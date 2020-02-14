2ba2d50560dc2f9d9e46ff22c5bfbfdbfe57f214
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-14 11:25:53 +0200

    Different types of Selectors

---
+++ scripts/buttons.js
@@ -0,0 +1,14 @@
+console.log("Started buttons.js");
+
+//i can store all buttons
+//this will be an array of button elements
+const btns = document.querySelectorAll("button");
+const btnsAgain = document.getElementsByTagName("button");
+const btnsAlso = document.getElementsByClassName("btn");
+
+
+//individual buttons
+const b1 = document.querySelector("#b-1");
+const b2 = document.querySelector(".btn-2");
+const b3 = document.getElementById("b-3");
+
